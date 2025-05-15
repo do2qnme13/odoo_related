03249aa1b43ba23db2fafea95b44012f972bbaa4
Psh <pyaesonehein81998@gmail.com>
2025-05-15 22:02:14 +0630

    [ADD] chart.js

--- custom_sale_dashboard/static/src/components/sales_dashborad.js
+++ custom_sale_dashboard/static/src/components/sales_dashborad.js
@@ -1,8 +1,55 @@
 import { registry } from "@web/core/registry";
 import { KpiCard } from "./kpi_card/kpi_card";
-import { Component, useState, onWillStart, useRef } from "@odoo/owl";
+import { Component, useState, onWillStart, useRef, onMounted } from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
 
-export class OwlSalesDashboard extends Component {}
+export class OwlSalesDashboard extends Component {
+    setup() {
+        this.charRef = useRef("chart");
+        console.log(this.charRef);
+        onWillStart(async () => {
+            await loadJS(
+                "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js"
+            );
+        });
+
+        onMounted(() => {
+            const data = [
+                { year: 2010, count: 10 },
+                { year: 2011, count: 20 },
+                { year: 2012, count: 15 },
+                { year: 2013, count: 25 },
+                { year: 2014, count: 22 },
+                { year: 2015, count: 30 },
+                { year: 2016, count: 28 },
+            ];
+
+            new Chart(this.charRef.el, {
+                type: "doughnut",
+                options: {
+                    animation: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        tooltip: {
+                            enabled: false,
+                        },
+                    },
+                },
+                data: {
+                    labels: data.map((row) => row.year),
+                    datasets: [
+                        {
+                            label: "Acquisitions by year",
+                            data: data.map((row) => row.count),
+                        },
+                    ],
+                },
+            });
+        });
+    }
+}
 
 OwlSalesDashboard.template = "owl.OwlSalesDashboard"; // Your QWeb template name
 OwlSalesDashboard.components = { KpiCard };
