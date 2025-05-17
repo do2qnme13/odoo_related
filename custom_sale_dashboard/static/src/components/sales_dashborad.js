e39fb8962da2e1bfe77503caea357f4fadaed95f
Psh <pyaesonehein81998@gmail.com>
2025-05-17 12:25:31 +0630

    [ADD]

--- custom_sale_dashboard/static/src/components/sales_dashborad.js
+++ custom_sale_dashboard/static/src/components/sales_dashborad.js
@@ -2,55 +2,10 @@ import { registry } from "@web/core/registry";
 import { KpiCard } from "./kpi_card/kpi_card";
 import { Component, useState, onWillStart, useRef, onMounted } from "@odoo/owl";
 import { loadJS } from "@web/core/assets";
+import { ChartRenderer } from "./chart_render/chart_renderer";
 
-export class OwlSalesDashboard extends Component {
-    setup() {
-        this.charRef = useRef("chart");
-        console.log(this.charRef);
-        onWillStart(async () => {
-            await loadJS(
-                "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js"
-            );
-        });
-
-        onMounted(() => {
-            const data = [
-                { year: 2010, count: 10 },
-                { year: 2011, count: 20 },
-                { year: 2012, count: 15 },
-                { year: 2013, count: 25 },
-                { year: 2014, count: 22 },
-                { year: 2015, count: 30 },
-                { year: 2016, count: 28 },
-            ];
-
-            new Chart(this.charRef.el, {
-                type: "doughnut",
-                options: {
-                    animation: false,
-                    plugins: {
-                        legend: {
-                            display: false,
-                        },
-                        tooltip: {
-                            enabled: false,
-                        },
-                    },
-                },
-                data: {
-                    labels: data.map((row) => row.year),
-                    datasets: [
-                        {
-                            label: "Acquisitions by year",
-                            data: data.map((row) => row.count),
-                        },
-                    ],
-                },
-            });
-        });
-    }
-}
+export class OwlSalesDashboard extends Component {}
 
 OwlSalesDashboard.template = "owl.OwlSalesDashboard"; // Your QWeb template name
-OwlSalesDashboard.components = { KpiCard };
+OwlSalesDashboard.components = { KpiCard, ChartRenderer };
 registry.category("actions").add("owl.sales_dashboard", OwlSalesDashboard);
